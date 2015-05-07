456b1020eb75dd604eb1b8a5880f06826b493876
mateus.alves <mateus.alves@locaweb.com.br>
2015-05-07 19:55:25 -0300

    bdd e metas

---
+++ bdd_spec.rb
@@ -0,0 +1,289 @@
+require 'spec_helper'
+
+describe "na contratação de um IP adicional" do
+
+	describe "quando o produto principal é revenda plesk 11.5 linux" do
+		context "verifica valor do produto" do
+			xit "através da contratação pela central do cliente"
+			end
+			xit "através da contratação pelo cal"
+			end
+		end
+		context "contrata apenas um IP" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end	
+		end
+		context "contrata mais de um IP" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end		
+		end
+		context "realiza upgrade do serviço principal" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end	
+		end
+		context "desativa o serviço principal" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end    		
+		end
+		context "reativa o serviço principal" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end    		
+		end
+		context "cancela o serviço principal" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end    		
+		end
+	end
+
+	describe "quando o produto principal é revenda plesk 11.5 windows" do
+		context "verifica valor do produto" do
+			xit "através da contratação pela central do cliente"
+			end
+			xit "através da contratação pelo cal"
+			end
+			end
+		end
+		context "contrata apenas um IP" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end	
+		end
+		context "contrata mais de um IP" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end		
+		end
+		context "realiza upgrade do serviço principal" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end	
+		end
+		context "desativa o serviço principal" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end    		
+		end
+		context "reativa o serviço principal" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end    		
+		end
+		context "cancela o serviço principal" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end    		
+		end
+	end
+
+	describe "quando o produto principal é revenda cPanel" do
+		context "verifica valor do produto" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end
+		end
+		context "contrata apenas um IP" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end	
+		end
+		context "contrata mais de um IP" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end		
+		end
+		context "realiza upgrade do serviço principal" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end	
+		end
+		context "desativa o serviço principal" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end    		
+		end
+		context "reativa o serviço principal" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end    		
+		end
+		context "cancela o serviço principal" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end    		
+		end
+	end
+
+end
+
+describe "na atualização da versão do ruby no reseller-provisioning" do
+
+	describe "quando o produto for plesk 11.5 linux" do
+		context "contrata a revenda" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end 		
+		end 
+		context "contrata um serviço adicional" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end     		
+		end
+		context "desativa a revenda" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end    		
+		end
+		context "reativa a revenda" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end   		
+		end
+		context "cancela a revenda" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end     		
+		end
+	end
+
+	describe "quando o produto for revenda plesk 11.5 windows" do
+		context "contrata a revenda" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end 		
+		end 
+		context "contrata um serviço adicional" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end     		
+		end
+		context "desativa a revenda" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end    		
+		end
+		context "reativa a revenda" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end   		
+		end
+		context "cancela a revenda" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end     		
+		end
+	end
+
+	context "quando o produto for revenda cpanel" do
+		context "contrata a revenda" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end 		
+		end 
+		context "contrata um serviço adicional" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end     		
+		end
+		context "desativa a revenda" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end    		
+		end
+		context "reativa a revenda" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end   		
+		end
+		context "cancela a revenda" do
+			xit "através da central do cliente"
+			end
+			xit "através do cal"
+			end     		
+		end
+	end
+
+end
+
+describe "na melhoria no procedimento do reseller-migration" do
+
+	context "quando acessar a ferramenta de migração" do
+		xit "acessa a interface melhorada" do
+		end
+		xit "migrar e-mails de uma revenda plesk 11.0 para revenda plesk 11.5" do
+		end
+		xit "refazer migração apenas para e-mails que apresentarem falha" do
+		end
+	end
+
+end
+
+describe "na migração de clientes do Sys I para Sys II com serviços adicionais" do
+
+	context "quando tem apenas um serviço adicional contratado" do
+		xit "migra cliente com IP adicional" do
+		end
+		xit "migra cliente com espaço adicional" do
+		end
+		xit "migra cliente com banco de dados adicional" do
+		end
+	end
+
+	context "quando tem mais de um serviço adicional contratado" do
+		xit "migra cliente com IP adicional, espaço adicional, e banco de dados adicional" do
+		end
+		xit "migra cliente com IP adicional e espaço adicional" do
+		end
+		xit "migra cliente com espaço adicional e banco de dados adicional" do
+		end
+		xit "migra cliente com espaço adicional e banco de dados adicional" do
+		end
+	end
+
+end
+
+describe "no estudo de viabilidade do parallels automation" do
+
+	context "quando acoplar um serviço" do
+		xit "acopla o office 365" do
+		end
+
+end
